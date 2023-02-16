6514f1be80bccf92b6a5710e0f6a26753ea29390
divine016 <koutidivine@gmail.com>
2023-02-16 01:58:16 +0100

    changes made task for task 0X0D

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- a script that list all the records of the table second_table of the database hbtn_0c_0 in your my sql server
-SELECT score, name , FROM second_table  WHERE name IS NOT NULL ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL AND name != '' ORDER BY score DESC;
