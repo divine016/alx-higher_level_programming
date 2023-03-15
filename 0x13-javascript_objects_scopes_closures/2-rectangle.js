4173622b6ecca6ee18807250e4b597a8af2215d0
divine016 <koutidivine@gmail.com>
2023-03-15 17:18:59 +0100

    task from like 0-2

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+class Rectangle{
+    constructor(w, h){
+        if(w <= 0 || h <= 0){
+            const empty ={}
+        }else{
+            this.width = w;
+            this.height = h;
+        }
+        
+    }
+    
+}
